84819a8b8eabe0ee5df92c686da1dd174edffa07
Adam Makarucha <adammakarucha@gmail.com>
2020-06-11 01:32:44 +1000

    Added initial class for dabbing notebook image state

--- nbextender/nbextender.py
+++ nbextender/nbextender.py
@@ -1,6 +1,30 @@
 from kubeflow.fairing.preprocessors.base import BasePreProcessor
 
 
+class NBExtender(object):
+	def __init__(self, new_image, base_image):
+		self.image_name = new_image
+		self.base_image = base_image
+		
+	def get_global_state(self):
+		# get current image
+		# self.base_image 
+		# get minio config
+		# get arch
+		
+	def get_
+		
+		
+		
+	def save(self):
+		# get current state
+		## python installs ubuntu installs
+		# send current state to builder
+		
+
+
+
+
 class NotebookExtenderPreProcessor(BasePreProcessor):
     """ The notebook externder preprocess for the context that comes from BasePreProcessor.
     : param BasePreProcessor: a context that gets sent to the builder for the docker build and 
