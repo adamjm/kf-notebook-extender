dd0723ebe3c221db4b08c4c1516e1fc2aef6c757
Adam Makarucha <adammakarucha@gmail.com>
2020-06-11 01:50:52 +1000

    added minio context

--- nbextender/nbextender.py
+++ nbextender/nbextender.py
@@ -1,18 +1,31 @@
 from kubeflow.fairing.preprocessors.base import BasePreProcessor
-
+from kubeflow.fairing.builders.cluster.minio_context import MinioContextSource
 
 class NBExtender(object):
-	def __init__(self, new_image, base_image):
+	def __init__(self, new_image, base_image, context_source_type=None):
 		self.image_name = new_image
 		self.base_image = base_image
 		
+	  if context_source_type is None:
+	  	raise RuntimeError("context_source_type is not specified")
+    self.context_source_type = context_source_type
+        
 	def get_global_state(self):
 		# get current image
 		# self.base_image 
 		# get minio config
 		# get arch
 		
-	def get_
+	def set_context_source(self):
+		if self.context_source_type == "minio":
+		  endpoint_url = os.environ.get("MINIO_URL")
+		  minio_secret=os.environ.get("ACCESSKEY")
+		  minio_secret_key=os.environ.get("SECRETKEY")
+      region_name=os.environ.get("REGION")
+			self.context_source =	MinioContextSource( endpoint_url, minio_secret, minio_secret_key,
+                 region_name)
+      
+		
 		
 		
 		
